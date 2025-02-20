b1c62b7f4280e5ef4efc6e4bde59bf131bc849b0
Aziza Solace Afadhali <s.afadhali@alustudent.com>
2025-02-20 17:35:42 +0200

    this is my portifolio

---
+++ CSS/javascript.js/about.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector(".expand_me").addEventListener("click", () => {
+        const more_details = document.querySelector(".extended_other");
+        const expand =  document.querySelector(".expand_me");
+        if (more_details.classList.contains("hidden")) {
+            more_details.classList.remove("hidden");
+            more_details.classList.add("show");
+            expand.textContent = "show less";
+        } else {
+            more_details.classList.remove("show");
+            more_details.classList.add("hidden");
+            expand.textContent = "More About Me";
+        }
+    });
+    document.querySelector(".more_details").addEventListener("click", () => {
+        const more_details2 = document.querySelector(".more_project1");
+        const expand2 =  document.querySelector(".more_details");
+        if (more_details2.classList.contains("hidden")) {
+            more_details2.classList.remove("hidden");
+            more_details2.classList.add("show");
+            expand2.textContent = "Less Details";
+        } else {
+            more_details2.classList.remove("show");
+            more_details2.classList.add("hidden");
+            expand2.textContent = "More Details";
+        }
+});
+    document.querySelector(".form2").addEventListener("submit", (e) => {
+        console.log("Form submitted, but preventDefault should stop it.");
+        e.preventDefault();
+        const first_name = document.getElementById("first_name").value;
+        const second_name = document.getElementById("second_name").value;
+        const email = document.getElementById("email").value;
+        const text_area = document.getElementById("textarea").value;
+        alert(`The Message is Sent By ${second_name} ${first_name}`);
+    });
+});
